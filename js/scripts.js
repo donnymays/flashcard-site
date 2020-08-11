46055d02b704ccd7e540609cae0c1879d3df6e03
3Emme <emmettbuentiempo@gmail.com>
2020-08-11 15:03:11 -0700

    expand click/toggle function between card terms and definitions from all cards at once to all cards individually.
    co-authored-by: Donny Mays <dtmays2@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,126 @@
 $(document).ready(function() {
-  $("h5.card-term").click(function() {
-    $(".card-term").toggle();
-    $(".card-definition").toggle();
+  $("h5.card-term-1").click(function() {
+    $(".card-term-1").toggle();
+    $(".card-definition-1").toggle();
   });
 });
 
 $(document).ready(function() {
-  $("p.card-definition").click(function() {
-    $(".card-term").toggle();
-    $(".card-definition").toggle();
+  $("p.card-definition-1").click(function() {
+    $(".card-term-1").toggle();
+    $(".card-definition-1").toggle();
   });
 });
+
+$(document).ready(function() {
+  $("h5.card-term-2").click(function() {
+    $(".card-term-2").toggle();
+    $(".card-definition-2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-2").click(function() {
+    $(".card-term-2").toggle();
+    $(".card-definition-2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("h5.card-term-3").click(function() {
+    $(".card-term-3").toggle();
+    $(".card-definition-3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-3").click(function() {
+    $(".card-term-3").toggle();
+    $(".card-definition-3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("h5.card-term-4").click(function() {
+    $(".card-term-4").toggle();
+    $(".card-definition-4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-4").click(function() {
+    $(".card-term-4").toggle();
+    $(".card-definition-4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("h5.card-term-5").click(function() {
+    $(".card-term-5").toggle();
+    $(".card-definition-5").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-5").click(function() {
+    $(".card-term-5").toggle();
+    $(".card-definition-5").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("h5.card-term-6").click(function() {
+    $(".card-term-6").toggle();
+    $(".card-definition-6").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-6").click(function() {
+    $(".card-term-6").toggle();
+    $(".card-definition-6").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("h5.card-term-7").click(function() {
+    $(".card-term-7").toggle();
+    $(".card-definition-7").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-7").click(function() {
+    $(".card-term-7").toggle();
+    $(".card-definition-7").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("h5.card-term-8").click(function() {
+    $(".card-term-8").toggle();
+    $(".card-definition-8").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-8").click(function() {
+    $(".card-term-8").toggle();
+    $(".card-definition-8").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("h5.card-term-9").click(function() {
+    $(".card-term-9").toggle();
+    $(".card-definition-9").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p.card-definition-9").click(function() {
+    $(".card-term-9").toggle();
+    $(".card-definition-9").toggle();
+  });
+});
+
